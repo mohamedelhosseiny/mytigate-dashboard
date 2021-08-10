f90e7964d4eda8d97f9358fd9c3e4906aa25d307
hosseiny <elhosseiny@nbhood.com>
2021-08-10 13:57:04 +0200

    feat: add footer layout

--- components/layout/mytigateFooter.vue
+++ components/layout/mytigateFooter.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="bg-pearl w-full">hi</div>
+  <footer class="bg-pearl w-full sm:flex-row flex flex-col px-section py-20">
+    <div class="mb-20 mr-30">
+      <ul class="text-deep-blue">
+        <li>Version 1.1.0</li>
+        <li><a href="mailto:support@mytigate.com">Support@mytigate.com</a></li>
+        <li>Technical Documentation</li>
+      </ul>
+    </div>
+    <div>
+      <ul class="text-deep-blue">
+        <li>&copy; Copyright 2019 - {{ new Date().getFullYear() }}</li>
+        <li><a href="mailto:support@mytigate.com">Mytigate Gmbh</a></li>
+        <li>All rights reserved</li>
+      </ul>
+    </div>
+  </footer>
 </template>
 
 <script>
