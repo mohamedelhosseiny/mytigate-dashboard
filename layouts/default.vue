ad1dbf8883b7ab80fb60f24d4789c029ebcea966
hosseiny <elhosseiny@nbhood.com>
2021-08-10 11:40:47 +0200

    refactor: remove nuxt defaults

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+// import nuxtHeader from "@/components/layout/nuxtHeader.vue";
+// import nuxtFooter from "@/components/layout/nuxtFooter.vue";
+
+export default {
+  head: {
+    // link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+  },
+  components: {
+    // nuxtFooter,
+    // nuxtHeader,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <div>hi</div>
 </template>
 
 <script>
