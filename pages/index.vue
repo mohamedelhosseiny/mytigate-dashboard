b183a9dae560ae166059e893144f54f81097a4ac
hosseiny <elhosseiny@nbhood.com>
2021-08-10 14:17:49 +0200

    feat: add homepage ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="flex-grow">
-    <h1 class="text-deep-blue">deep blue</h1>
+  <div class="flex mt-20 md:mt-30 justify-center flex-grow">
+    <h1 class="sr-only">Homepage</h1>
+    <h2 class="text-deep-blue font-bold text-center mx-section-md text-1">
+      Welcome To
+      <img
+        src="http://mytigate.de/wp-content/uploads/2019/10/Logo-MYTIGATE@2x.png"
+        alt="Mytigate"
+      />
+    </h2>
   </div>
 </template>
 
