ea9d3269a140e1f68d2f69bd490ea2a7f9996d9a
hosseiny <elhosseiny@nbhood.com>
2021-08-10 15:21:35 +0200

    feat: add basic auto complete functionality

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,35 +1,17 @@
 <template>
-  <div class="flex">
-    <div class="relative w-full search">
-      <input
-        placeholder="Search..."
-        class="rounded-l pr-15 py-10 border w-full"
-        type="text"
-      />
-      <svg-component class="search_icon" icon="search" />
-    </div>
-    <button class="bg-blue text-white rounded py-10 px-15">Search</button>
+  <div>
+    <search-form />
+    <!-- <resu /> -->
   </div>
 </template>
 
 <script>
-export default {}
+import searchForm from '@/components/dashboard/searchForm.vue'
+export default {
+  components: {
+    searchForm,
+  },
+}
 </script>
 
-<style lang="scss" scoped>
-.search {
-  input {
-    padding-left: 35px;
-    &:focus {
-      outline-color: rgba(0, 0, 0, 0.2);
-    }
-  }
-  &_icon {
-    position: absolute;
-    left: 10px;
-    top: 50%;
-    transform: translateY(-50%);
-    z-index: 999;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
