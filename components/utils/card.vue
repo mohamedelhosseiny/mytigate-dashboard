e8525a7e1f97c330735666ee38212749dcb15251
hosseiny <elhosseiny@nbhood.com>
2021-08-11 22:40:51 +0200

    remove reference object

--- components/utils/card.vue
+++ components/utils/card.vue
@@ -24,16 +24,6 @@
         Deaths:
         <strong>{{ new Intl.NumberFormat().format(country.deaths) }}</strong>
       </p>
-      <!-- { "updated": 1628607707617, "country": "France", "countryInfo": { "_id":
-        250, "iso2": "FR", "iso3": "FRA", "lat": 46, "long": 2, "flag":
-        "https://disease.sh/assets/img/flags/fr.png" }, "cases": 6310933,
-        "todayCases": 0, "deaths": 112288, "todayDeaths": 0, "recovered":
-        5777774, "todayRecovered": 0, "active": 420871, "critical": 1667,
-        "casesPerOneMillion": 96449, "deathsPerOneMillion": 1716, "tests":
-        110018641, "testsPerOneMillion": 1681391, "population": 65433111,
-        "continent": "Europe", "oneCasePerPeople": 10, "oneDeathPerPeople": 583,
-        "oneTestPerPeople": 1, "activePerOneMillion": 6432.08,
-        "recoveredPerOneMillion": 88300.46, "criticalPerOneMillion": 25.48 } -->
     </nuxt-link>
   </div>
 </template>
