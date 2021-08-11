002f10b8437001f43e62950433bcb5963ef52d86
hosseiny <elhosseiny@nbhood.com>
2021-08-11 22:06:17 +0200

    feat: add chart.js library

--- pages/countries/_slug/index.vue
+++ pages/countries/_slug/index.vue
@@ -1,11 +1,48 @@
 <template>
   <div>
     {{ $route.params.slug }}
+    <div class="canvas_wrapper">
+      <canvas id="chart"></canvas>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Chart from 'chart.js/auto'
+
+export default {
+  async asyncData({ $axios, params }) {
+    const result = await $axios.get(`/historical/${params.slug}?lastdays=30`)
+    return {
+      country: result.data,
+    }
+  },
+  mounted() {
+    var canvas = document.getElementById('chart')
+
+    var chart = new Chart(canvas, {
+      type: 'line',
+      data: {
+        labels: Object.keys(this.country.timeline.cases),
+        datasets: [
+          {
+            data: Object.values(this.country.timeline.cases),
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
+    })
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+canvas {
+  width: 100%;
+  height: 300px;
+  border: 1px solid black;
+}
+</style>
