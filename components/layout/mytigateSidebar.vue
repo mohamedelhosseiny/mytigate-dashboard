c3218d1041893269fcf5647ae3540cc9dbc4c388
hosseiny <elhosseiny@nbhood.com>
2021-08-11 12:57:23 +0200

    style: add navburger layout

--- components/layout/mytigateSidebar.vue
+++ components/layout/mytigateSidebar.vue
@@ -1,6 +1,8 @@
 <template>
   <div
     class="
+      sidebar
+      relative
       bg-deep-blue
       h-full
       pt-20
@@ -20,6 +22,10 @@
         <nuxt-link class="text-white" to="/dashboard">Dashboard</nuxt-link>
       </li>
     </ul>
+    <div class="sidebar_burger">
+      <div class="sidebar_burger_bar mb-5"></div>
+      <div class="sidebar_burger_bar"></div>
+    </div>
   </div>
 </template>
 
@@ -30,5 +36,26 @@ export default {}
 <style lang="scss" scoped>
 .nuxt-link-exact-active {
   font-weight: bold;
+}
+.sidebar {
+  &_burger {
+    position: absolute;
+    top: 40px;
+    right: 0px;
+    padding: 10px;
+    background-color: var(--deep-blue);
+    transform: translateX(100%);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    &_bar {
+      height: 2px;
+      width: 15px;
+      background-color: white;
+      &:last-of-type() {
+        margin-bottom: 0px;
+      }
+    }
+  }
 }
 </style>
